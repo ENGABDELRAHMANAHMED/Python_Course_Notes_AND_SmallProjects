f39de634134caedb4ec91c7a0848b9b336e1c194
ENGABDELRAHMANAHMED <eng.abdelrahman.ahmed26@gmail.com>
2025-07-22 09:36:16 +0300

    22 July 2025

---
+++ AbdelrahmanModule.py
@@ -0,0 +1,9 @@
+def SayHello(name) :
+    print(f"- Hello {name}")
+    
+def SayHowAreYou(name) :
+    print(f"- How are you {name} ?")
+    
+def SayHowOldAreYou(name) :
+    print(f"- How old are you {name} ?")
+    
